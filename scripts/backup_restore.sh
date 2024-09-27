9cc0802d48c40562968c440d2351cf996424f2dc
abvanliew <ab.vanliew@gmail.com>
2024-09-26 22:14:34 -0400

    restoring data

--- scripts/backup_restore.sh
+++ scripts/backup_restore.sh
@@ -1,2 +1,5 @@
 mongodump --db relic --out /data/backup/`date +"%Y-%m-%d"`
-mongorestore --db relic-restore /data/backup/`date +"%Y-%m-%d"`/relic
+mongorestore --db relic /data/backup/`date +"%Y-%m-%d"`/relic
+
+podman exec mongo-db bash -c 'mongodump --db relic --out /data/backup/`date +"%Y-%m-%d"`'
+podman exec mongo-db bash -c 'mongorestore --db relic /data/backup/`date +"%Y-%m-%d"`/relic'

a24c68825e98e2e27705e3a3ae2f11ef92a68061
abvanliew <ab.vanliew@gmail.com>
2024-12-28 23:54:53 -0500

    more updates

--- scripts/backup_restore.sh
+++ scripts/backup_restore.sh
@@ -1,9 +1,17 @@
 # create new mongo pod
 podman kube play --replace ./kube/mongo.yaml
 
+# start podman
+podman machine start
+
+# start mongodb
+podman start mongo-db
+
+# backcommand when in pod shell
 mongodump --db relic --out /data/backup/`date +"%Y-%m-%d"`
 mongorestore --db relic /data/backup/`date +"%Y-%m-%d"`/relic
 
-podman exec mongo-db bash -c 'mongodump --db relic --out /data/backup/`date +"%Y-%m-%d"`'
+# quick exec commands
+podman exec mongo-db bash -c 'mongodump --db relic --out /data/backup/`date +"%Y-%m-%d"` && echo `date +"%Y-%m-%d"`'
 podman exec mongo-db bash -c 'mongorestore --db relic /data/backup/`date +"%Y-%m-%d"`/relic'
-podman exec mongo-db bash -c 'mongorestore --db relic /data/backup/2024-09-28/relic'
+podman exec mongo-db bash -c 'mongorestore --db relic /data/backup/2024-12-18/relic'
--- src/main.rs
+++ src/main.rs
@@ -1,79 +1,38 @@
-#![allow(non_snake_case)]
-
 mod skills;
 
 use skills::Skill;
-
-use dioxus::prelude::*;
-use dioxus_logger::tracing::{info, Level};
 use std::error::Error;
-use tokio;
+use std::fmt::{Display, Formatter};
 
 use mongodb::{Client, Collection};
 use mongodb::bson::doc;
+use bson::oid::ObjectId;
 use futures::stream::StreamExt;
 
-use axum::{response::Html, routing::get, Router};
-use dioxus::prelude::*;
-
-fn main() {
-  dioxus_logger::init( Level::INFO ).expect( "failed to init logger" );
-  info!( "starting app" );
-  launch( App );
+#[tokio::main]
+async fn main() {
+  let _x = get_skills().await;
 }
 
-fn App() -> Element {
-  rsx! {
-    Router::<Route> {}
+async fn get_skills() -> Result<(), Box<dyn Error>> {
+  let client = Client::with_uri_str("mongodb://localhost:27017").await?;
+  let skills_collection: Collection<Skill> = client.database("relic").collection("skills_paths");
+  let Ok( id ) = ObjectId::parse_str( "66f45f6b7e098103fc4220a1" ) else { return Err( Box::new(SkillError) ); };
+  let mut results = skills_collection
+  .find( doc! { "_id": id }, ).await?;
+  while let Some(doc) = results.next().await {
+    println!("{:?}", doc?)
+  }
+  Ok(())
+}
+
+#[derive(Debug, Clone)]
+struct SkillError;
+
+impl Display for SkillError {
+  fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+    write!( f, "unable to find skill" )
   }
 }
 
-#[derive(Clone, Routable, Debug, PartialEq)]
-enum Route {
-  #[route("/")]
-  Home {},
-  #[route("/skill/:id")]
-  Skill { id: String },
-}
-
-#[component]
-fn Skill( id: String ) -> Element {
-  rsx! {
-    Link { to: Route::Home {}, "Go to counter" }
-    "Skill {id}"
-  }
-}
-
-#[component]
-fn Home() -> Element {
-  let mut count = use_signal(|| 0);
-
-  rsx! {
-    Link {
-      to: Route::Skill { id: "id".into() },
-      "Go to skill"
-    }
-    div {
-      h1 { "Count: {count}" }
-      button { onclick: move |_| count += 1, "Up" }
-      button { onclick: move |_| count -= 1, "Down" }
-    }
-  }
-}
-
-// #[tokio::main]
-// async fn main() -> Result<(), Box<dyn Error>> {
-//   let client = Client::with_uri_str("mongodb://localhost:27017").await?;
-
-//   let skills_collection: Collection<Skill> = client.database("relic").collection("skills_paths");
-
-//   let mut results = skills_collection
-//   .find(
-//     doc! {},
-//   ).await?;
-//   while let Some(doc) = results.next().await {
-//     println!("{:?}", doc?)
-//   }
-
-//   Ok(())
-// }
+impl Error for SkillError {}

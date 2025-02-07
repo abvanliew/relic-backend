d11f482ac6caed407e3c219b6bad00e0afb62a52
abvanliew <ab.vanliew@gmail.com>
2025-02-06 23:35:47 -0500

    more updates

--- scripts/backup_restore.sh
+++ scripts/backup_restore.sh
@@ -16,3 +16,4 @@ podman exec mongo-db bash -c 'mongodump --db relic --out /data/backup/`date +"%Y
 podman exec mongo-db bash -c 'mongodump --db relic --out /data/backup/main'
 podman exec mongo-db bash -c 'mongorestore --db relic /data/backup/`date +"%Y-%m-%d"`/relic'
 podman exec mongo-db bash -c 'mongorestore --db relic /data/backup/2024-12-18/relic'
+podman exec mongo-db bash -c 'mongorestore --db relic /data/backup/main/relic'

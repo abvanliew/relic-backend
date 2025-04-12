9350c9a6175fbbbfddf24cb3ea641c1ea6e12b28
abvanliew <ab.vanliew@gmail.com>
2025-04-12 02:01:39 -0400

    whoops forgot about main backup

--- scripts/backup_restore.sh
+++ scripts/backup_restore.sh
@@ -12,7 +12,7 @@ mongodump --db relic --out /data/backup/`date +"%Y-%m-%d"`
 mongorestore --db relic /data/backup/`date +"%Y-%m-%d"`/relic
 
 # quick exec commands
-podman exec mongo-db bash -c 'mongodump --db relic --out /data/backup/`date +"%Y-%m-%d"` && echo `date +"%Y-%m-%d"`'
+# podman exec mongo-db bash -c 'mongodump --db relic --out /data/backup/`date +"%Y-%m-%d"` && echo `date +"%Y-%m-%d"`'
 podman exec mongo-db bash -c 'mongodump --db relic --out /data/backup/main'
 podman exec mongo-db bash -c 'mongorestore --db relic /data/backup/`date +"%Y-%m-%d"`/relic'
 podman exec mongo-db bash -c 'mongorestore --db relic /data/backup/2024-12-18/relic'

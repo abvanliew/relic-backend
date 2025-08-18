1250ccf86a1d2118f43f5dc5bdfb69d2578be1cf
abvanliew <ab.vanliew@gmail.com>
2025-08-18 19:31:23 -0400

    Skill Format Refactor (#1)
    * skill updates
    
    * more changes
    
    * before upsert
    
    * refactoring updates
    
    * more skill refactoring

--- scripts/backup_restore.sh
+++ scripts/backup_restore.sh
@@ -7,6 +7,8 @@ podman machine start
 # start mongodb
 podman start mongo-db
 
+podman exec -it mongo-db bash
+
 # backcommand when in pod shell
 mongodump --db relic --out /data/backup/`date +"%Y-%m-%d"`
 mongorestore --db relic /data/backup/`date +"%Y-%m-%d"`/relic
@@ -14,6 +16,13 @@ mongorestore --db relic /data/backup/`date +"%Y-%m-%d"`/relic
 # quick exec commands
 # podman exec mongo-db bash -c 'mongodump --db relic --out /data/backup/`date +"%Y-%m-%d"` && echo `date +"%Y-%m-%d"`'
 podman exec mongo-db bash -c 'mongodump --db relic --out /data/backup/main'
+podman exec mongo-db bash -c 'mongoexport -d relic -c skills -o /data/backup/skills.json'
+
 # podman exec mongo-db bash -c 'mongorestore --db relic /data/backup/`date +"%Y-%m-%d"`/relic' 
 # podman exec mongo-db bash -c 'mongorestore --db relic /data/backup/2024-12-18/relic'
 podman exec mongo-db bash -c 'mongorestore --db relic /data/backup/main/relic'
+podman exec mongo-db bash -c 'mongorestore --drop --db relic /data/backup/main/relic'
+
+
+mongoexport -d relic -c skills -o /data/backup/skills.json
+mongoimport --db=relic --collection=skills --file=/data/backup/upsert.json --mode upsert
---
+++ scripts/refactor.py
@@ -0,0 +1,75 @@
+import json
+
+
+GROUPED_KEYS = [ "text", "term", "roll" ]
+
+def load_json_objects(file_path: str)->list[dict]:
+  items=[]
+  with open(file_path, mode= "r") as file_io:
+    for line in file_io:
+      items.append(json.loads(line))
+  return items
+
+def write_json_objects(file_path:str, objects:list[dict]):
+  with open(file_path,"w") as file_io:
+    for object in objects:
+      file_io.write(f"{json.dumps(object)}\n")
+
+def refactor(skills: list[dict]):
+  refactored_skills=[]
+  for skill in skills:
+    new_skill = refactor_skill(skill)
+    if new_skill != skill:
+      refactored_skills.append(new_skill)
+  return refactored_skills
+
+def refactor_skill(skill:dict):
+  refactored_skill = {}
+  action = skill.get("action")
+  if action is not None:
+    refactored_skill["action"] = refactor_action(action)
+  
+  sub_actions = skill.get("subActions")
+  if sub_actions is not None:
+    refactored_sub_actions = []
+    for sub_action in sub_actions:
+      refactored_sub_actions.append( refactor_action(sub_action) )
+    refactored_skill["subActions"] = refactored_sub_actions
+  return {**skill,**refactored_skill}
+
+def refactor_action(action:dict):
+  refactored_action = {}
+  condition = action.get("condition")
+  if condition is not None:
+    refactored_action["condition"] = group_rules_block(condition)
+  rules = action.get("rules")
+  if rules is not None:
+    refactored_action["rules"] = group_rules_block(rules)
+  return {**action,**refactored_action}
+
+def group_rules_block(elements: list[dict]):
+  refactored = []
+  current_set = []
+  for element in elements:
+    if overlap(GROUPED_KEYS, element.keys()):
+      current_set.append(element)
+    else:
+      if len(current_set) > 0:
+        refactored.append({"block": current_set})
+        current_set = []
+      refactored.append( element )
+  if len(current_set) > 0:
+    refactored.append({"block": current_set})
+  return refactored
+
+def overlap(left: list, right: list)->bool:
+  for l in left:
+    for r in right:
+      if l == r:
+        return True
+  return False
+
+skills=load_json_objects("./data/mongo/backup/skills.json")
+# skills=load_json_objects("./hurl.json")
+refactors = refactor(skills)
+write_json_objects("upsert.json",refactors)
